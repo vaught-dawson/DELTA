4676bfdf129bad5285fa8d1dc6ddc3dd21158d17
Dawson Vaught <vaught.dawson@gmail.com>
2020-11-23 03:14:25 -0700

    Official v1.1.0 release.

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { description, version } = require('../package.json');
 
 module.exports = {
 	name: 'info',
@@ -10,7 +11,7 @@ module.exports = {
 		const embed = new Discord.MessageEmbed()
 			.setColor(15105570)
 			.setThumbnail('https://i.ibb.co/2MHY6wn/D-E-L-T-A-4.jpg')
-			.setTitle('Document Editing Logistics Technical Aid (DELTA):')
+			.setTitle(`${description} v${version}:`)
 			.setDescription(
 				'**Bot:** Made by Vio\n**Art:** Made by Ragnarok\n**Testing:** Conducted on RU (Lead by NeonJohn)'
 			)

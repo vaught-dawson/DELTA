87d9f44f09dbe7ecfda8dd91a3c607275a792ecd
Dawson Vaught <vaught.dawson@gmail.com>
2021-01-11 21:16:44 -0700

    Adapted 'addMemberToSheet.'

--- functions/addMemberToSheet.js
+++ functions/addMemberToSheet.js
@@ -1,10 +1,7 @@
 async function addMemberToSheet(member, sheet) {
 	await sheet.addRow({
 		Name: member.name,
-		Rank: '-01-TR',
-		Status: 'ACTIVE',
 		Discord: member.id || 'None',
-		Currency: 0
 	});
 }
 

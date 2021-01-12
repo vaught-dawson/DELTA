a42b244be51559bf3c8d0a2a53719b0639087607
Dawson Vaught <vaught.dawson@gmail.com>
2021-01-11 22:00:00 -0700

    + Renamed 'rankSystems' to 'rankStructures.'

--- commands/rankStructures.js
+++ commands/rankStructures.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const fs = require ('fs');
 
 module.exports = {
-	name: 'ranksystems',
+	name: 'rankstructures',
 	aliases: [ 'ranks' ],
 	description: 'Lists the available rank systems to choose from.',
 	args: false,
@@ -15,11 +15,11 @@ module.exports = {
             .setFooter('Resistance Logistics', 'https://i.ibb.co/Wzd001F/677a08d8682923ca8cb51fe48df38208.png');
         
         let path = '.\\information\\ranks'
-        const rankSystems = fs.readdirSync(path).filter((file) => file.endsWith('.json'));
+        const rankStructures = fs.readdirSync(path).filter((file) => file.endsWith('.json'));
 
         let desc = [];
-        rankSystems.forEach((system) => {
-            desc.push(system.substring(0, system.length - 5));
+        rankStructures.forEach((structure) => {
+            desc.push(structure.substring(0, structure.length - 5));
         })
         embed.setDescription(desc.join(', '));
 

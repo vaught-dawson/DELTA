30b06b6cf00e7fc120a99738b677cc8d8c0b8019
Dawson Vaught <vaught.dawson@gmail.com>
2021-01-12 18:04:05 -0700

    Reformatted output.

--- functions/getSheetInfo.js
+++ functions/getSheetInfo.js
@@ -2,12 +2,12 @@ const { getSheetHeaders } = require('./getSheetHeaders.js');
 
 async function getSheetInfo(sheet) {
 	var output = [];
-	output.push({ name: 'Id:', value: sheet.spreadsheetId });
-	output.push({ name: 'Rows:', value: sheet.rowCount });
-	output.push({ name: 'Columns:', value: sheet.columnCount });
-	output.push({ name: 'Type:', value: sheet.sheetType });
+	output.push({ name: 'Id:', value: sheet.spreadsheetId, inline: true });
+	output.push({ name: 'Rows:', value: sheet.rowCount, inline: true });
+	output.push({ name: 'Columns:', value: sheet.columnCount, inline: true });
+	output.push({ name: 'Type:', value: sheet.sheetType, inline: true });
 	let headers = await getSheetHeaders(sheet);
-	output.push({ name: 'Headers:', value: headers.join(', ') });
+	output.push({ name: 'Headers:', value: headers.join(', '), inline: false });
 	return output;
 }
 

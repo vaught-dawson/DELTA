93efca2879bdf27e91b57947a6cc9777897212b5
Dawson Vaught <vaught.dawson@gmail.com>
2021-01-17 09:07:59 -0700

    Fixed path.

--- commands/rankStructures.js
+++ commands/rankStructures.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require ('fs');
+const path = require('path');
 
 module.exports = {
 	name: 'rankstructures',
@@ -14,8 +15,8 @@ module.exports = {
 			.setTitle(`Avaliable rank structures:`)
             .setFooter('Resistance Logistics', 'https://i.ibb.co/Wzd001F/677a08d8682923ca8cb51fe48df38208.png');
         
-        let path = '.\\information\\ranks'
-        const rankStructures = fs.readdirSync(path).filter((file) => file.endsWith('.json'));
+		let dirPath = path.resolve('./information/ranks');
+        const rankStructures = fs.readdirSync(dirPath).filter((file) => file.endsWith('.json'));
 
         let desc = [];
         rankStructures.forEach((structure) => {

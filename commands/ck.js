025e1473de0094de4a7c1fd907e76ab8f9d193ea
Dawson Vaught <vaught.dawson@gmail.com>
2021-01-27 12:03:28 -0700

    Created generated logs.

--- commands/ck.js
+++ commands/ck.js
@@ -1,8 +1,7 @@
-const { addMemberToSheet } = require('../functions/addMemberToSheet.js');
+const { getMemberFromSheetById } = require('../functions/getMemberFromSheetById.js');
+const { getMemberFromSheetByName } = require('../functions/getMemberFromSheetByName.js');
 const { combineElementsByCharacter } = require('../functions/combineElementsByCharacter.js');
 const { getDiscordMember } = require('../functions/getDiscordMember.js');
-const { getMemberInfo } = require('../functions/getMemberInfo.js');
-const { isNameOnSheet } = require('../functions/isNameOnSheet.js');
 const { loadSpreadsheet } = require('../functions/loadSpreadsheet.js');
 const { removeMemberFromSheet } = require('../functions/removeMemberFromSheet.js');
 const { sendErrorEmbed } = require('../functions/sendErrorEmbed.js');
@@ -46,15 +45,35 @@ module.exports = {
 		if (!rosterSheet) {
             return message.channel.send('Invalid roster sheet name! Make sure you set it up properly in the config.');
         }
-        
+
+        var outputLog = '';
         for (let i = 0; i < members.length; i++) {
             try {
+                var memberData = await getMemberFromSheetById(members[i], rosterSheet, server);
+
+                if (!memberData) {
+                    memberData = await getMemberFromSheetByName(members[i], rosterSheet, server);
+
+                    if (!memberData) {
+                        return message.channel.send(
+                            `Member \`${members[i].name == null ? members[i].id : members[i].name}\` not found on the roster!`
+                        );
+                    }
+                }
+                
+                outputLog += `\n\`${server.memberLogPrefix} ${memberData[server.rankHeader].split('-').pop()} ${memberData[
+                    server.nameHeader
+                ]} -> CK\``;
+
                 message.channel.send(await removeMemberFromSheet(members[i], rosterSheet, server));
+
             } catch(err) {
                 console.log(err);
                 await sendErrorEmbed(message, { message: `**Command:** ${message.content}\n**Error:** ${err}` });
                 message.channel.send(`Failed to run \`ck\` on \`${members[i].name}\`.`);
             }
         }
+
+        return message.channel.send(outputLog);
 	}
 };
